bd1816ae14386af7564306f3900b9eaad836ad53
gaohui <gaohui@meizu.com>
2018-08-24 21:26:40 +0800

    支付：完善细节

--- Lagou/src/2018-offer-class/js/m_index.js
+++ Lagou/src/2018-offer-class/js/m_index.js
@@ -17,15 +17,28 @@ $(document).ready(function() {
             $('#ios-pay').removeClass('opacityChange-out').addClass('opacityChange').css({
                 'display':'block'
             })
-            $('.pay-tips,.pay-btn').css({
+            $('.pay-tips,.weixin-ercode').css({
                 'display':'block'
             })
+            $('.pay-btn').css({
+                'display':'block'
+            }).removeClass('opacityChange-out').addClass('opacityChange delay0-5');
             $('.pay-center').removeClass('zoomY-out').addClass('zoomY-in').css({
                 'display':'block'
             })
             flag = false;
+            postEncodingID({
+                "data-lg-tj-id":"1c5c" ,
+				"data-lg-tj-no":"0010" ,
+				"data-lg-tj-cid":"idnull"  
+            })
             return
         }
+        postEncodingID({
+            "data-lg-tj-id":"1c5c" ,
+            "data-lg-tj-no":"0011" ,
+            "data-lg-tj-cid":"idnull"  
+        })
         ajax(_url, _data, 'post', function(data) {
             flag = false;
             window.location.href = data.payUrl;
@@ -40,12 +53,13 @@ $(document).ready(function() {
         e.preventDefault();
 
         $('.pay-center').removeClass('zoomY-in').addClass('zoomY-out')
-        $('.pay-tips,.pay-btn').css({
+        $('.pay-tips,.weixin-ercode').css({
             'display':'none'
         })
+        $('.pay-btn').removeClass('opacityChange delay0-5').addClass('opacityChange-out');
         $('#ios-pay').removeClass('opacityChange').addClass('opacityChange-out')
         setTimeout(function(){
-            $('#ios-pay').css({
+            $('#ios-pay,.pay-btn').css({
                 'display':'none'
             })
         },500)
@@ -111,26 +125,6 @@ function setMenu() {
     function setMenuWithScroll (scrollTop) {
         if (flag) {
             return
-        }
-        if (scrollTop > scrollListMenu[12]) {
-            setAcive(12);
-            return;
-        }
-        if (scrollTop > scrollListMenu[11]) {
-            setAcive(11);
-            return;
-        }
-        if (scrollTop > scrollListMenu[10]) {
-            setAcive(10);
-            return;
-        }
-        if (scrollTop > scrollListMenu[9]) {
-            setAcive(9);
-            return;
-        }
-        if (scrollTop > scrollListMenu[8]) {
-            setAcive(8);
-            return;
         }
         if (scrollTop > scrollListMenu[7]) {
             setAcive(7);
@@ -201,4 +195,85 @@ function ajax(_url, _data, type, callFuc, errorFuc){
 			alert('出了个小问题, 请稍后再试');
 		}
 	})
+}
+
+function postEncodingID(data) {
+	var _PID = "",
+	_PNO = "",
+	_PV = 0,
+	_PCONTENTID = "",
+	_PRANDOM = "",
+	_GAMETHOD = "",
+	_GAMSGTYPE = "",
+	_GATJ = "",
+	_GATJVAL = 0,
+	v = 1,
+	t = "a",
+	dl = encodeURIComponent(window.location.href),
+	dr = document.referrer,
+	dt = document.title;
+    var _ELS = document;
+    var arr_GATJ = [];
+    try {
+        _PID = data["data-lg-tj-id"] || "idnull"
+        _PNO = data["data-lg-tj-no"] || "idnull"
+        _PCONTENTID = data["data-lg-tj-cid"] || "idnull"
+        _PRANDOM = getRandom()
+        _GAMETHOD = data["data-lg-gatj-method"] || "send"
+        _GAMSGTYPE = data["data-lg-gatj-msgtype"] || "event"
+        _GATJ = data["data-lg-gatj-msg"] || ""
+        _GATJVAL = parseInt(data["data-lg-gatj-val"] || 0)
+        if (_GATJ && typeof ga == "function") {
+            arr_GATJ.push(_GAMETHOD, _GAMSGTYPE);
+            arr_GATJ = arr_GATJ.concat(_GATJ.split(",")); !! _GATJVAL && arr_GATJ.push(_GATJVAL);
+            ga.apply(null, arr_GATJ)
+        }
+        if (_PID != "idnull") {
+            var _params = {};
+            _params.v = v;
+            _params.t = 'div';//target.tagName.toLocaleLowerCase();
+            _params.dl = dl;  
+            _params.dr = dr;
+            _params.dt = dt;
+            _params.aid = [_PID, _PNO, _PV, _PCONTENTID, _PRANDOM].join("_");
+            imgGET(_params)
+        } else {
+            return
+        }
+    } catch(e) {}
+}
+function imgGET(params) {
+    var REMOTE = {
+		server: "https://a.lagou.com/track"
+	};
+    var _img = new Image();
+    paramsArr = [];
+    for (var item in params) {
+        if (typeof item == "string") {
+            paramsArr.push(item + "=" + params[item])
+        }
+    }
+    _img.src = REMOTE.server + "?" + paramsArr.join("&")
+}
+function getRandom(digit) {
+    window._CASH_RANDOM ? "": window._CASH_RANDOM = {};
+    var _cash_random = window._CASH_RANDOM || {},
+    _digit = digit || 4,
+    _random = getRandomSimple();
+    while (_cash_random[_random]) {
+        _random = getRandomSimple();
+        if (!_cash_random[_random]) {
+            break
+        }
+    }
+    window._CASH_RANDOM[_random] = _random;
+    return _random;
+    function getRandomSimple() {
+        var random = "";
+        for (var i = 0; i < _digit; i++) {
+            var r = Math.floor(Math.random() * 10);
+            random += r.toString()
+        }
+        return random.toString()
+    }
 }
