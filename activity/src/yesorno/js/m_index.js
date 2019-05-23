6a50d8418e1a446d140f8ca16a4f86c79e2e1a8b
花伊浓 <943235816@qq.com>
2019-05-23 23:06:03 +0800

    Update m_index.js
    添加分享

--- activity/src/yesorno/js/m_index.js
+++ activity/src/yesorno/js/m_index.js
@@ -9,6 +9,12 @@ var app = new Vue({
         
     },
     methods:{
-        
+        toShare: function(){
+            console.log(123455)
+            if(WBAPP && WBAPP.action && WBAPP.action.share){
+                WBAPP.action.share(shareConfigAry, function(){
+                console.log('分享成功！')
+            })
+        }
     }
 })
