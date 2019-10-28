79f99314d1a2a0196907c009edffc8396398d2ab
花伊浓 <943235816@qq.com>
2019-10-28 08:40:45 +0800

    Update loading.js

--- Lagou/src/2019-vote/js/loading.js
+++ Lagou/src/2019-vote/js/loading.js
@@ -1,6 +1,6 @@
 // JavaScript Document
-// var loadingHost = '';
-var loadingHost = 'https://www.lgstatic.com/activity-rsrc/dist/2018-vote/';
+var loadingHost = '';
+// var loadingHost = 'https://www.lgstatic.com/activity-rsrc/dist/2018-vote/';
 var imgArray = [
 	loadingHost+"images/loading-bg.png",
 	loadingHost+"images/loading-complete.png",
